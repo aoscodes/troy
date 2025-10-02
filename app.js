8230ae53ff8fba2e0b3ed00417d61fe5ad148db5
tyler <tyler@aos.codes>
2025-10-02 08:36:00 +0000

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 80
+app.get('/', (req, res) => {
+  res.sendFile("/home/spiralling/web/views/hellen.html")
+})
+
+app.get('/hellen', (req, res) => {
+  res.redirect('/false/hellen')
+})
+
+app.get('/false/hellen', (req, res) => {
+  res.sendFile("/home/spiralling/web/views/hellen.html")
+})
+
+app.get('/true/hellen', (req, res) => {
+  res.sendFile("/home/spiralling/web/views/hellen.txt")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
